2c34eac8b950d7ade224afa34601b37e20383054
Zunw <Sasa25B@seznam.cz>
2022-09-12 22:00:23 +0200

    Small change

--- utils/scripts/getUserID.php
+++ utils/scripts/getUserID.php
@@ -2,9 +2,11 @@
 
 function getUserIDFromUser()
 {
+    session_start();
     include "../TodoList/utils/scripts/connectToDatabase.php";
-
-    $username = $_SESSION['login_user'];
+    if (isset($_SESSION['login_user'])) { 
+        $username = $_SESSION['login_user']; 
+    };
     $userIDQuery = "select userID from user where username = '$username'";
     $userIDFetch = mysqli_fetch_assoc(mysqli_query($conn, $userIDQuery));
     $userID = $userIDFetch['userID'];
