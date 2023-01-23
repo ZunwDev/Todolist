82870864509676a0fdd9084b7ce568c3a9a85e10
Zunw <Sasa25B@seznam.cz>
2023-01-23 08:22:43 +0100

    Some hotfix for sidebar

--- utils/js/main_page_setup.js
+++ utils/js/main_page_setup.js
@@ -1,61 +1,61 @@
 function setClassToBeggining(element, classToAdd) {
-  let splittedClass = $(`#${element}`).attr('class').split(' ');
-  splittedClass.unshift(classToAdd);
-  document.getElementById(element).classList = splittedClass.join(' ');
+	let splittedClass = $(`#${element}`).attr('class').split(' ');
+	splittedClass.unshift(classToAdd);
+	document.getElementById(element).classList = splittedClass.join(' ');
 }
 
 function expandSidebar() {
-  if ($('#sidebar').hasClass('shrinkSidebar')) {
-    show('sidebar');
-    let sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('shrinkSidebar');
-    setClassToBeggining('sidebar', 'expandSidebar');
-    setTimeout(() => {
-      $('.app_appProjectsContainer').addClass('pl-64');
-    }, 100);
-    return;
-  }
-  sidebar.classList.remove('expandSidebar');
-  setClassToBeggining('sidebar', 'shrinkSidebar');
-  setTimeout(() => {
-    $('.app_appProjectsContainer').removeClass('pl-64');
-    $('#sidebar').hide();
-  }, 100);
+	if ($('#sidebar').hasClass('shrinkSidebar')) {
+		show('sidebar');
+		let sidebar = document.getElementById('sidebar');
+		sidebar.classList.remove('shrinkSidebar');
+		setClassToBeggining('sidebar', 'expandSidebar');
+		setTimeout(() => {
+			$('.app_appProjectsContainer').addClass('pl-64');
+		}, 200);
+		return;
+	}
+	sidebar.classList.remove('expandSidebar');
+	setClassToBeggining('sidebar', 'shrinkSidebar');
+	setTimeout(() => {
+		$('.app_appProjectsContainer').removeClass('pl-64');
+		$('#sidebar').hide();
+	}, 200);
 }
 
 function toggleAngle() {
-  const upClass = 'toggle-up';
-  const downClass = 'toggle-down';
+	const upClass = 'toggle-up';
+	const downClass = 'toggle-down';
 
-  const angle = document.querySelector('.angle');
-  angle.className = angle.classList.contains('toggle-down')
-    ? angle.classList.replace(downClass, upClass)
-    : angle.classList.replace(upClass, downClass);
+	const angle = document.querySelector('.angle');
+	angle.className = angle.classList.contains('toggle-down')
+		? angle.classList.replace(downClass, upClass)
+		: angle.classList.replace(upClass, downClass);
 }
 
 function show(element) {
-  document.getElementById(element).style.display = 'block';
+	document.getElementById(element).style.display = 'block';
 }
 
 function showFlex(element) {
-  document.getElementById(element).style.display = 'flex';
+	document.getElementById(element).style.display = 'flex';
 }
 
 function URL(url) {
-  history.pushState(
-    {
-      id: 'TodoList',
-      source: 'web',
-    },
-    `TodoList`,
-    `http://localhost/TodoList/${url}`
-  );
+	history.pushState(
+		{
+			id: 'TodoList',
+			source: 'web',
+		},
+		`TodoList`,
+		`http://localhost/TodoList/${url}`
+	);
 }
 
 function title(title) {
-  document.title = title;
+	document.title = title;
 }
 
 const classToggle = (el, ...args) => {
-  args.map((e) => el.classList.toggle(e));
+	args.map((e) => el.classList.toggle(e));
 };
