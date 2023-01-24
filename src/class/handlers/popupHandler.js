98a589e8f7ec2e17e886afe06b13b20c61978d40
Zunw <Sasa25B@seznam.cz>
2023-01-24 14:12:38 +0100

    Fixed one thing with move to menu

--- src/class/handlers/popupHandler.js
+++ src/class/handlers/popupHandler.js
@@ -1,45 +1,48 @@
 class PopupHandler {
-  popupModalSettings(listener, overlay, element = 'popupElement') {
-    if (document.getElementById(element) != null) {
-      classToggle(document.getElementById(element), 'beforeShowUp', 'afterShowUp');
-      document.getElementById(overlay).addEventListener('pointerdown', listener);
-    }
-  }
+	popupModalSettings(listener, overlay, element = 'popupElement') {
+		if (document.getElementById(element) != null) {
+			classToggle(document.getElementById(element), 'beforeShowUp', 'afterShowUp');
+			document.getElementById(overlay).addEventListener('pointerdown', listener);
+		}
+	}
 
-  setToCorrectPos() {
-    let diff = document.body.scrollHeight - mouse.y;
-    let bheight = document.body.scrollHeight;
-    let mdiff = mouse.y + document.body.scrollHeight - diff;
+	setToCorrectPos() {
+		let diff = document.body.scrollHeight - mouse.y;
+		let bheight = document.body.scrollHeight;
+		let mdiff = mouse.y + document.body.scrollHeight - diff;
 
-    mdiff > bheight
-      ? document.getElementById('popupElement').classList.add(`top-[${mouse.y + window.scrollY - 128}px]`)
-      : document.getElementById('popupElement').classList.add(`top-[${mouse.y + window.scrollY}px]`);
-    document.getElementById('popupElement').classList.add(`left-[${mouse.x + window.scrollX + 16}px]`);
-  }
+		mdiff > bheight
+			? document.getElementById('popupElement').classList.add(`top-[${mouse.y + window.scrollY - 128}px]`)
+			: document.getElementById('popupElement').classList.add(`top-[${mouse.y + window.scrollY}px]`);
+		document.getElementById('popupElement').classList.add(`left-[${mouse.x + window.scrollX + 16}px]`);
+	}
 
-  showPopup(
-    htmlString,
-    setPosFunction,
-    closeAnyPopup,
-    closeSettings,
-    overlay = 'popupOverlay',
-    element = 'popupElement'
-  ) {
-    closeAnyPopup();
-    body.insertAdjacentHTML('beforeend', htmlString);
-    setPosFunction();
-    this.popupModalSettings(closeSettings, overlay, element);
-  }
+	showPopup(
+		htmlString,
+		setPosFunction,
+		closeAnyPopup,
+		closeSettings,
+		overlay = 'popupOverlay',
+		element = 'popupElement'
+	) {
+		closeAnyPopup();
+		body.insertAdjacentHTML('beforeend', htmlString);
+		setPosFunction();
+		this.popupModalSettings(closeSettings, overlay, element);
+	}
 
-  closeAnyPopup() {
-    if (document.getElementById('popupOverlay') != null) {
-      document.getElementById('popupOverlay').remove();
-    }
-  }
+	closeAnyPopup() {
+		if (document.getElementById('popupOverlay') != null) {
+			document.getElementById('popupOverlay').remove();
+		}
+		if (document.getElementById('popupPopupOverlay') != null) {
+			document.getElementById('popupPopupOverlay').remove();
+		}
+	}
 
-  closeModal(e) {
-    if (e.target === e.currentTarget) {
-      closeAnyPopup();
-    }
-  }
+	closeModal(e) {
+		if (e.target === e.currentTarget) {
+			closeAnyPopup();
+		}
+	}
 }
