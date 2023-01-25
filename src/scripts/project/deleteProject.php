c50ed22f480d3c2255846d154a2300e3e229c40d
Zunw <Sasa25B@seznam.cz>
2023-01-25 11:13:06 +0100

    Fix for project delete and made it better

--- src/scripts/project/deleteProject.php
+++ src/scripts/project/deleteProject.php
@@ -8,36 +8,28 @@ $IDQ = "select * from project where projectID = '$id' and userID = '$userID'";
 $result = mysqli_query($conn, $IDQ);
 
 if (mysqli_num_rows($result) > 0) {
-    $findBoard = "select boardID from board where projectID = '$id'";
-    $fetchBoard = mysqli_fetch_assoc(mysqli_query($conn, $findBoard));
-    $boardID = $fetchBoard["boardID"];
-
     $findBoardData = "select dataID from board_data where boardID = '$boardID'";
-    $fetchData = mysqli_fetch_assoc(mysqli_query($conn, $findBoardData));
-
+    $findBoard = "select boardID from board where projectID = '$id'";
     $findProjectLogs = "select activityID from project_activity where projectID = '$id'";
-    $fetchLogs = mysqli_fetch_assoc(mysqli_query($conn, $findProjectLogs));
 
     //Deleting tasks from boards if any exist
     if (mysqli_num_rows(mysqli_query($conn, $findBoardData)) > 0) {
-        $dataID = $fetchData["dataID"];
         if ($dataID != null || !empty($dataID)) {
-            $sql = "delete from board_data where dataID >= '$dataID' and projectID = '$id'";
+            $sql = "DELETE board_data FROM board_data JOIN board ON board_data.boardID = board.boardID WHERE board.projectID = '$id'";
             mysqli_query($conn, $sql);
         }
     }
     //Deleting board data from project if theres any
     if (mysqli_num_rows(mysqli_query($conn, $findBoard)) > 0) {
         if ($boardID != null || !empty($boardID)) {
-            $sql = "delete from board where boardID >= '$boardID' and projectID = '$id'";
+            $sql = "delete from board where projectID = '$id'";
             mysqli_query($conn, $sql);
         }
     }
     // Deleting the project activity logs. */
     if (mysqli_num_rows(mysqli_query($conn, $findProjectLogs)) > 0) {
-        $activityID = $fetchLogs['activityID'];
         if ($id != null || !empty($id)) {
-            $sql = "delete from project_activity where activityID >= '$activityID' and projectID = '$id'";
+            $sql = "delete from project_activity where projectID = '$id'";
             mysqli_query($conn, $sql);
         }
     }
